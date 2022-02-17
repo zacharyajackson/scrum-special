2d6aa1e233545699e2d1b3746406322faf712acc
Zachary Jackson <zachary.a.j23@gmail.com>
2022-02-16 20:46:39 -0500

    Added Feature that I Built Separately from a Tutorial
    Live search function for users.  There's some placeholder JSON data, but it can be easily replaced with user information that the students can input.  Possibly even play around with their own cards and personalize it.

---
+++ Roster/script.js
@@ -0,0 +1,31 @@
+const userCardTemplate = document.querySelector("[data-user-template")
+const UserCardContainer = document.querySelector("[data-user-cards-container]")
+const SearchInput = document.querySelector("[data-search]")
+
+let users = []
+
+SearchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    users.forEach(user=> {
+        const isVisibile = 
+        user.name.toLowerCase().includes(value) || 
+        user.email.toLowerCase().includes(value)
+        user.element.classList.toggle("hide", !isVisibile)
+
+    })
+})
+
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res =>  res.json())
+    .then(data => {
+        users =data.map(user => {
+            const card = userCardTemplate.content.cloneNode(true).children[0]
+            const header = card.querySelector("[data-header]") 
+            const body = card.querySelector ("[data-body]")
+            header.textContent = user.name
+            body.textContent = user.email
+            UserCardContainer.append(card)
+            return{ name: user.name, email: user.email, element: card }
+        })
+       
+    })
