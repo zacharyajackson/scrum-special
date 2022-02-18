b29a799d10e08e01746f25982bfd4f706e6801cf
HarrisMBrad <bmichaelh13@gmail.com>
2022-02-18 13:42:00 -0500

    accepting changes in scripts

--- Roster/script.js
+++ Roster/script.js
@@ -6,26 +6,26 @@ let users = []
 
 SearchInput.addEventListener("input", e => {
     const value = e.target.value.toLowerCase()
-    users.forEach(user=> {
-        const isVisibile = 
-        user.name.toLowerCase().includes(value) || 
-        user.email.toLowerCase().includes(value)
+    users.forEach(user => {
+        const isVisibile =
+            user.name.toLowerCase().includes(value) ||
+            user.email.toLowerCase().includes(value)
         user.element.classList.toggle("hide", !isVisibile)
 
     })
 })
 
 fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res =>  res.json())
+    .then(res => res.json())
     .then(data => {
-        users =data.map(user => {
+        users = data.map(user => {
             const card = userCardTemplate.content.cloneNode(true).children[0]
-            const header = card.querySelector("[data-header]") 
-            const body = card.querySelector ("[data-body]")
+            const header = card.querySelector("[data-header]")
+            const body = card.querySelector("[data-body]")
             header.textContent = user.name
             body.textContent = user.email
             UserCardContainer.append(card)
-            return{ name: user.name, email: user.email, element: card }
+            return { name: user.name, email: user.email, element: card }
         })
-       
+
     })
