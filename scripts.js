e4b2243ebc89c06b273b99834d76d7d98a9899b1
Brad M. Harris <bmichaelh13@gmail.com>
2022-03-03 17:22:59 -0500

    saying hello
    w/ this hello feature

--- scripts.js
+++ scripts.js
@@ -3,7 +3,6 @@ console.log('hello-world');
 
 // 2. let hello-world render in browser
 let helloWorld = new 'helloWorld';
-// 3. declare my Browser and asign it to a greeting function to call the string helloWorld camelCased.
+// 3. declare my Browser and assign it to a greeting function to call the string helloWorld camelCased.
 let myBrowser = greeting('helloWorld');
 greeting(myBrowser);
-console.log(...helloWorld);
