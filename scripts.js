30f8722a7353ec101584c4bcfc35f4c46849dad8
Brad M. Harris <bmichaelh13@gmail.com>
2022-03-07 10:01:31 -0500

    added object for functionality, arranged w/edits for futures

--- scripts.js
+++ scripts.js
@@ -1,8 +1,18 @@
-// 1. hello-world
-console.log('hello-world');
-
-// 2. let hello-world render in browser
-let helloWorld = new 'helloWorld';
-// 3. declare my Browser and assign it to a greeting function to call the string helloWorld camelCased.
+// 1. let hello-world render in browser
+function sayHello() {
+      console.log("Hello");
+}
+// 2. declare my Browser and assign it to a greeting function to call the string helloWorld camelCased.
 let myBrowser = greeting('helloWorld');
 greeting(myBrowser);
+// 3. Object 
+let user = {
+      name: "Lighthouse Larry",
+      e_mail: "hello@lighthouselabs.ca",
+      quack: function () {
+            console.log('QUACK!');
+      }
+     // Continue when ready
+
+};
+//4. 
